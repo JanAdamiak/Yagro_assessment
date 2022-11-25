657449ead101976213c0a54183c828fe2b29b63b
Jan Adamiak <jadamiak01@gmail.com>
2022-11-25 09:43:21 +0000

    setting up remote repo

---
+++ main.py
@@ -0,0 +1,5 @@
+from simulation import Simulation
+
+
+my_simulation = Simulation(5, 100, 3)
+my_simulation.run_simulation()
--- simulation.py
+++ simulation.py
@@ -2,41 +2,69 @@ from typing import Dict, List
 
 from conveyor_belt import ConveyorBelt
 from worker import Worker
-from enums import WorkerState
+from enums import WorkerState, FactoryItem, COMPONENTS
 
 
 class Simulation:
-    def __init__(self, length_of_conveyor_belt: int, steps: int, workers: Dict[int, List[Worker]]) -> None:
+    def __init__(self, length_of_conveyor_belt: int, steps: int, pairs_of_workers: int) -> None:
         self.conveyor_belt = ConveyorBelt(length_of_conveyor_belt)
         self.steps = steps
-        self.workers = workers
+        self.workers = self.place_workers_along_the_belt(pairs_of_workers)
         self.basket = {}
 
-        self.workers_requiring_cleanup: List[Worker]
+        self.workers_requiring_cleanup: List[Worker] = []
 
     def resolve_one_step_of_time(self) -> None:
         """Resolve actions equal to one unit of time (movement of the conveyor belt and worker actions)."""
         self.resolve_belt_action()
         self.resolve_worker_actions()
 
+        self.cleanup_workers()
+
+    def place_workers_along_the_belt(self, pairs_of_workers) -> Dict[int, List[Worker]]:
+        return {n:[Worker(), Worker()] for n in range(0, pairs_of_workers)}
+
     def resolve_belt_action(self) -> None:
         dropped_item = self.conveyor_belt.move_belt()
-        if dropped_item:
+        if dropped_item != FactoryItem.EMPTY_SPACE:
             self.basket[dropped_item] = self.basket[dropped_item] + 1
         
     def resolve_worker_actions(self) -> None:
         for belt_index, workers in self.workers.items():
-            for worker in workers:
-
-                if worker.current_state == WorkerState.ASSEMBLING:
-                    worker.continue_assembling()
-
             item = self.conveyor_belt.contents[belt_index]
-            if worker.current_state == WorkerState.LOOKING_FOR_PARTS and worker.is_part_desired_by_worker(item):
-                self.conveyor_belt.pick_up_item(belt_index)
-                worker.pick_up_part(item)
+            print("resolving worker actions:")
+            # print(workers)
+            print(item)
 
+            for worker in workers:
+                if worker.current_state == WorkerState.ASSEMBLING:
+                    is_worker_changing_state = worker.continue_assembling()
+                    if is_worker_changing_state:
+                        self.workers_requiring_cleanup.append(worker)
 
+            if item in COMPONENTS:
+                for worker in workers:
+                    if worker.current_state == WorkerState.LOOKING_FOR_PARTS and worker.is_part_desired_by_worker(item):
+                        self.conveyor_belt.pick_up_item(belt_index)
+                        is_worker_changing_state = worker.pick_up_part(item)
+                        if is_worker_changing_state:
+                            self.workers_requiring_cleanup.append(worker)
+                        break
+                        
+            elif item == FactoryItem.EMPTY_SPACE:
+                for worker in workers:
+                    if worker.current_state == WorkerState.WAITING_TO_DROP_ITEM:
+                        self.conveyor_belt.place_completed_item(belt_index)
+                        is_worker_changing_state = worker.drop_completed_item()
+                        if is_worker_changing_state:
+                            self.workers_requiring_cleanup.append(worker)
+                        break
+
+    def cleanup_workers(self) -> None:
+        for worker in self.workers_requiring_cleanup:
+            worker.set_new_state()
+
+        self.workers_requiring_cleanup = []
 
     def run_simulation(self) -> None:
         while self.steps > 0:
